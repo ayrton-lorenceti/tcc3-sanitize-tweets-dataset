6b2ba9d53eff0149828086c9fbfa449a2027e838
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2021-01-20 20:54:29 -0300

    [20-01-2021] Alterada função para obter somente id_str e text do tweet

--- src/utils/dynamodb_utils.py
+++ src/utils/dynamodb_utils.py
@@ -2,16 +2,8 @@ import boto3
 
 dynamodb = boto3.resource("dynamodb")
 
-def parse_urls_set_from_tweet_to_list(scanned_tweet):
-  scanned_tweet["urls"] = list(scanned_tweet["urls"])
-  
-  return scanned_tweet
-
-def parse_tweets_urls(scanned_tweets):
-  return [
-    parse_urls_set_from_tweet_to_list(scanned_tweet) for scanned_tweet in scanned_tweets 
-    if len(scanned_tweet["urls"]) > 0
-  ]
+def get_tweets_from_items(tweets):
+  return [{ "id_str": tweet["id_str"], "full_text": tweet["text"] } for tweet in tweets]
 
 def scan_tweets_table():
   table = dynamodb.Table('Tweets')
@@ -20,15 +12,15 @@ def scan_tweets_table():
   expression_attribute_values = { ":text": "RT @" }
   expression_attribute_names = { "#text": "text" }
 
-  scanned_tweets = table.scan(
+  tweets = table.scan(
     FilterExpression=filter_expression,
     ExpressionAttributeValues=expression_attribute_values,
     ExpressionAttributeNames=expression_attribute_names
   )
 
   return {
-    "count": scanned_tweets["Count"],
-    "items": parse_tweets_urls(scanned_tweets["Items"]),
-    "last_evaluated_key": scanned_tweets["LastEvaluatedKey"],
-    "scanned_count": scanned_tweets["ScannedCount"]
+    "count": tweets["Count"],
+    "tweets": get_tweets_from_items(tweets["Items"]),
+    "last_evaluated_key": tweets["LastEvaluatedKey"],
+    "scanned_count": tweets["ScannedCount"]
   }
