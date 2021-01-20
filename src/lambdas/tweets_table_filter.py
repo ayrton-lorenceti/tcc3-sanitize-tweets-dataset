4d936c0ac74179df74301010c9e4baa70b6e5bc8
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2021-01-20 08:52:42 -0300

    [19-01-2021] Criação do s3_utils

--- src/lambdas/tweets_table_filter.py
+++ src/lambdas/tweets_table_filter.py
@@ -1,7 +1,12 @@
-import boto3
+from utils.s3_utils import read_json_from_s3
 
 def lambda_handler(event, context):
-  return {
-    "status": 200,
-    "message": "Ok"
-  }
+  try:
+    read_json_from_s3()
+    
+    return {
+      "status": 200,
+      "message": "Ok"
+    }    
+  except Exception as error:
+    return error
