8b321b242f2b6dcda3daf9b464d6a2565aa67789
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2021-01-20 21:55:38 -0300

    [20-01-2021] Adicionada função para salvar o valor de LastEvaluatedKey no JSOn

--- src/utils/s3_utils.py
+++ src/utils/s3_utils.py
@@ -1,7 +1,17 @@
 import boto3
+import json
 
 s3 = boto3.resource("s3")
 object = s3.Object("tcc3bucket", "last_evaluated_key.json")
 
 def read_json_from_s3():
   return object.get()['Body'].read().decode('utf-8')
+
+def save_last_evaluated_key_to_json(last_evaluated_key):
+  data = {
+    "last_evaluated_key": last_evaluated_key
+  }
+
+  object.put(
+    Body=((json.dumps(data).encode('UTF-8')))
+  )
