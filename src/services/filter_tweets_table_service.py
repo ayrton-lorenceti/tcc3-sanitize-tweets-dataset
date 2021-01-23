f6c821299bdf97474a35e17a98a0985f44889b33
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2021-01-22 23:15:41 -0300

    [22-01-2021] Criado service para filtrar tweets a partir do início da tabela Tweets

---
+++ src/services/filter_tweets_table_service.py
@@ -0,0 +1,14 @@
+from utils.dynamodb_utils import scan_tweets_table_without_pagination
+from utils.s3_utils import save_last_evaluated_key_to_json
+
+def filter_from_the_start():
+  filtered_tweets = scan_tweets_table_without_pagination()
+
+  save_last_evaluated_key_to_json(filtered_tweets["last_evaluated_key"])
+
+  print("count:", filtered_tweets["count"])
+  print("last_evaluated_key:", filtered_tweets["last_evaluated_key"]["id_str"])
+  print("scanned_count:", filtered_tweets["scanned_count"])
+
+  return filtered_tweets
+
--- src/utils/dynamodb_utils.py
+++ src/utils/dynamodb_utils.py
@@ -5,24 +5,21 @@ from pprint import pprint
 from utils.s3_utils import save_last_evaluated_key_to_json
 
 dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table('Tweets')
+
+filter_expression = "NOT begins_with(#text, :text)"
+expression_attribute_values = { ":text": "RT @" }
+expression_attribute_names = { "#text": "text" }
 
 def get_tweets_from_items(tweets):
   return [{ "id_str": tweet["id_str"], "full_text": tweet["text"] } for tweet in tweets]
 
-def scan_tweets_table():
-  table = dynamodb.Table('Tweets')
-  
-  filter_expression = "NOT begins_with(#text, :text)"
-  expression_attribute_values = { ":text": "RT @" }
-  expression_attribute_names = { "#text": "text" }
-
+def scan_tweets_table_without_pagination():
   tweets = table.scan(
     FilterExpression=filter_expression,
     ExpressionAttributeValues=expression_attribute_values,
     ExpressionAttributeNames=expression_attribute_names
   )
-
-  save_last_evaluated_key_to_json(tweets["LastEvaluatedKey"]["id_str"])
 
   filtered_tweets = {
     "count": tweets["Count"],
@@ -30,9 +27,5 @@ def scan_tweets_table():
     "last_evaluated_key": tweets["LastEvaluatedKey"],
     "scanned_count": tweets["ScannedCount"]
   }
-  
-  print("count:", filtered_tweets["count"])
-  print("last_evaluated_key:", filtered_tweets["last_evaluated_key"]["id_str"])
-  print("scanned_count:", filtered_tweets["scanned_count"])
 
   return filtered_tweets
