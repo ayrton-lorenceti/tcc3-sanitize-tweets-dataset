12d25d4ffd1929c2ded048ee293fa966af4722d6
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2021-01-25 20:54:27 -0300

    [25-01-2021] Correção para enviar o parâmetro last_evaluated_key

--- src/lambdas/filter_tweets_table.py
+++ src/lambdas/filter_tweets_table.py
@@ -5,6 +5,6 @@ def lambda_handler(event, context):
   try:
     last_evaluated_key = read_json_from_s3()
     
-    return filter_by_last_evaluated_key()
+    return filter_by_last_evaluated_key(last_evaluated_key)
   except:
     return filter_from_the_start()
