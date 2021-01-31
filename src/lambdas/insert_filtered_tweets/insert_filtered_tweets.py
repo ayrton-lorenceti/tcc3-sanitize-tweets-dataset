d63cfc9dd59c16a26928112f96823eede0399de7
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2021-01-31 17:28:44 -0300

    [31-01-2021] Forçando o pipe

--- src/lambdas/insert_filtered_tweets/insert_filtered_tweets.py
+++ src/lambdas/insert_filtered_tweets/insert_filtered_tweets.py
@@ -3,5 +3,4 @@ from core.services.insert_filtered_tweets_service import insert_filtered_tweets
 def lambda_handler(event, context):
   tweets = event["tweets"]
 
-
   return insert_filtered_tweets(tweets)
