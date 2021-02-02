2c6395c59d6195bfb1bc2152070162c285441cc6
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2021-02-02 19:29:24 -0300

    [02-02-2021] Adição de try/except no insert_filtered_tweets

--- src/insert_filtered_tweets.py
+++ src/insert_filtered_tweets.py
@@ -3,4 +3,7 @@ from services.insert_filtered_tweets_service import insert_filtered_tweets
 def lambda_handler(event, context):
   tweets = event["tweets"]
 
-  return insert_filtered_tweets(tweets)
+  try: 
+    return insert_filtered_tweets(tweets)
+  except Exception as error:
+    return error
