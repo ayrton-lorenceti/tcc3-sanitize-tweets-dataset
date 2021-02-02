ebaee2814e5c336ef26290af1e1bbf81a9429153
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2021-02-02 15:18:20 -0300

    [02-02-2021] Adição de prints

--- src/utils/ssm_utils.py
+++ src/utils/ssm_utils.py
@@ -2,7 +2,7 @@ print("antes de importar o boto3 - ssm_utils")
 import boto3
 print("depois de importar o boto3 - ssm_utils")
 client = boto3.client('ssm')
-
+print("depois de importar o client ssm - ssm_utils")
 def get_parameters(parameter_name):
   parameters_response = client.get_parameters(
     Names=[parameter_name],
