d8ae13c30f62181a7ea46c13a5ab3fbbeedc061b
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2021-04-01 23:48:45 -0300

    [01-04-2021] Adição de try catch para capturar erro ao obter json do s3

--- src/lambdas/remove_incomplete_tweets.py
+++ src/lambdas/remove_incomplete_tweets.py
@@ -1,5 +1,14 @@
+from utils.s3_utils import read_json_from_s3
+import boto3
+
+s3 = boto3.client("s3")
+
 def lambda_handler(event, context):
-  return {
-    "status": 200,
-    "message": "Ok"
-  }
+  try:
+    last_evaluated_key = read_json_from_s3()
+    
+    return last_evaluated_key
+  except s3.exceptions.NoSuchKey as exception:
+    pass
+  except Exception as exception:
+    print(exception)
